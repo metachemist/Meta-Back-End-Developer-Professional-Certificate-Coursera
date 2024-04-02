c526cd126fa897f5ecc987eed712025147d300a7
metachemist <meta.chemist.22@gmail.com>
2024-04-02 21:04:54 +0500

    Updating the names of folder and removing error in ordering_system

--- Course 02-Programming in Python/Week2-Basic programming in python/ordering_system.py
+++ Course 02-Programming in Python/Week2-Basic programming in python/ordering_system.py
@@ -6,44 +6,42 @@ menu = {
     5: {"name": 'sandwich', "price": 4.99},    
 }
 
-#program starts executing from here
-if __name__ == "__main__"
-    main()  #this will call main function!
-
 #01
 def main():
     order = take_order()
     print_order(order)
 
     subtotal = calculate_subtotal(order)
-    print("Subtotal for the order is:")
+    print("Subtotal for the order is: " + str(subtotal))
 
     tax = calculate_tax(subtotal)
     print("Tax for the order is: " + str(tax))
 
-    items, subtotal = summarize_order(order)
+    items, total = summarize_order(order)
+    print("You have ordered: ", items)
+    print("Total amount to pay (including tax): $" + str(total))
+
 
 #03 take order calls display menu
 def display_menu():
     print(" ----- Menu ----- ")
     for selection in menu:
-        print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price']} : >5")
+        print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price']} ")
     print()
 
 #02
 def take_order():
     display_menu()
     order = []
-    count = 1
-    for i in range(3):
-        item = input('select menu item number ' + str(count) + '(from 1 to 5):')  
-        count +=1
-        order.extend(menu[int(item)])
+    for _ in range(3):
+        item = int(input('Select menu item number (from 1 to 5): '))
+        order.append(menu[item])  # Append the selected item directly
     return order
+
 
 # print_order called by main function
 def print_order(order):
-    print("You have ordered" + str(len(order)) + "items")
+    print("You have ordered " + str(len(order)) + " items")
     items = []
     items = [item["name"] for item in order]
     print(items)
@@ -66,3 +64,7 @@ def calculate_subtotal(order):
     """ Calculates the subtotal of an order """
     subtotal = sum(item["price"] for item in order)
     return subtotal
+
+#program starts executing from here
+if __name__ == "__main__":
+    main()  #this will call main function!
