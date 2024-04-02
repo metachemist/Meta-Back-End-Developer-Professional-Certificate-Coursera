67ac808e1ce4e7ef5dd3617a778fc799b4c9450c
metachemist <meta.chemist.22@gmail.com>
2024-04-02 21:33:44 +0500

    Added multiple folder func

--- Course 02-Programming in Python/Week2-Basic programming in python/filehandling.py
+++ Course 02-Programming in Python/Week2-Basic programming in python/filehandling.py
@@ -5,3 +5,15 @@ f_content_list = f_content.split("\n")
 print(f_content_list)
 f.close()
 print(random.choice(f_content_list))
+
+#Finally, If I had multiple files in my folder, I could allow myself to pick a file from which to read in a list of names.
+
+#Here's how that would work:
+
+import random
+f_name = input('Type the file name: ')
+f = open(f_name) # "r" omitted as it's the default
+f_content = f.read()
+f_content_list = f_content.split("\n")
+f.close()
+print(random.choice(f_content_list))
